b824b82c07ced5184292b006783d00d93ffcb378
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2019-11-08 16:21:40 +0330

    fix: typo
    Signed-off-by: Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>

--- script/ci-test.sh
+++ script/ci-test.sh
@@ -8,8 +8,8 @@
 # -x  Print commands and their arguments as they are executed.
 set -ex
 
-chmod +x script/test-style
-chmod +x script/test-module
+chmod +x script/test-style.sh
+chmod +x script/test-module.sh
 
 script/test-style
 script/test-module

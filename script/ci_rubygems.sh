18a58ce7780af42d442c7c82855334f138acf2cd
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2019-12-03 20:46:47 +0330

    modify: -ex flag added
    Signed-off-by: Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>

--- script/ci_rubygems.sh
+++ script/ci_rubygems.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+# Usage: script/ci_rubygems.sh
+#
+# Create credential file for rubygems.org.
+
+# set flag for shell execution.
+# -e  Exit immediately if a command exits with a non-zero status.
+# -x  Print commands and their arguments as they are executed.
+set -ex
+
 mkdir ~/.gem
 echo -e "---/r/n:rubygems_api_key: $RUBTGEMS_API_KEY" > ~/.gem/credentials
 chmod 0600 ~/.gem/credentials

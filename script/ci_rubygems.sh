7bef9522a8ea2f0a444c3ef49b9db00148f72e98
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2019-12-03 21:40:00 +0330

    ci: debug
    Signed-off-by: Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>

--- script/ci_rubygems.sh
+++ script/ci_rubygems.sh
@@ -5,11 +5,8 @@
 
 # set flag for shell execution.
 # -e  Exit immediately if a command exits with a non-zero status.
-# -x  Print commands and their arguments as they are executed.
-set -ex
+set -e
 
 mkdir -p ~/.gem
-echo -e "---/r/n:mahdi_baghbani: ${RUBYGEMS_API_KEY}" > ~/.gem/credentials
+echo -e "---/r/n:rubygems_api_key: ${RUBYGEMS_API_KEY}" > ~/.gem/credentials
 chmod 0600 ~/.gem/credentials
-ls ~/.gem
-cat ~/.gem/credentials

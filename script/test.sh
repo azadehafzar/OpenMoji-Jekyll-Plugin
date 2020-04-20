a2c760fdc1e70a21eb79d8c28bbddd3358d8ce5f
Mohammad Mahdi Baghbani Pourvahid <MahdiBaghbani@protonmail.com>
2020-04-20 22:32:34 +0430

    modify: test.sh can be invoked inside script directory
    Signed-off-by: Mohammad Mahdi Baghbani Pourvahid <MahdiBaghbani@protonmail.com>

--- script/test.sh
+++ script/test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Usage: script/ci_test.sh
 #
-# Runs tests.
+# run all the tests.
 
 # set flag for shell execution.
 # -e  Exit immediately if a command exits with a non-zero status.
 # -x  Print commands and their arguments as they are executed.
 set -ex
 
-script/test_style.sh
-script/test_module.sh
+script/test_style.sh || ./test_style.sh
+script/test_module.sh || ./test_module.sh

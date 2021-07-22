f130c4928e9b574e8c951635cfec5667c0b1a398
Mohammad Mahdi Baghbani Pourvahid <MahdiBaghbani@protonmail.com>
2021-07-22 17:56:47 +0430

    modify: re-organize whole updater for supporting multiple image extensions

--- script/updater.py
+++ script/updater.py
@@ -12,31 +12,38 @@ from preview import create_md_files
 # configure path variables.
 file_path = os.path.abspath(os.path.dirname(__file__))
 base_path = os.path.abspath(os.path.dirname(file_path))
-image_directory = os.path.join(base_path, "images", "color", "svg")
-document_directory = os.path.join(base_path, "doc", "blog")
 temp_directory = os.path.join(file_path, "temp")
-svg_path = os.path.join(temp_directory, "openmoji")
+document_directory = os.path.join(base_path, "doc", "blog")
 negarmoji_emoji_json = os.path.join(temp_directory, "negarmoji.json")
 negarmoji_filenames_json = os.path.join(temp_directory, "filenames.json")
+
+image_directory = os.path.join(base_path, "images")
+color_image_directory = os.path.join(image_directory, "color")
+black_image_directory = os.path.join(image_directory, "black")
+
+openmoji_svg_color_path = os.path.join(temp_directory, "openmoji-svg-color")
+openmoji_svg_black_path = os.path.join(temp_directory, "openmoji-svg-black")
 
 # download latest files from negarmoji and openmoji repositories.
 process = subprocess.run(["./downloader.sh"], stdout=subprocess.PIPE)
 
-# update docs.
-create_md_files(negarmoji_emoji_json, document_directory)
-
 # verify filenames.
-rename(svg_path, negarmoji_filenames_json)
+rename(openmoji_svg_color_path, "svg", negarmoji_filenames_json, 1)
+rename(openmoji_svg_black_path, "svg", negarmoji_filenames_json, 2)
 
 # delete current svg in the image directory.
 process = subprocess.run(["rm", "-rf", f"{image_directory}"], stdout=subprocess.PIPE)
 
-# create new svg directory.
-process = subprocess.run(["mkdir", "-p", f"{image_directory}"], stdout=subprocess.PIPE)
+# create new directories.
+process = subprocess.run(["mkdir", "-p", f"{color_image_directory}"], stdout=subprocess.PIPE)
+process = subprocess.run(["mkdir", "-p", f"{black_image_directory}"], stdout=subprocess.PIPE)
 
-# move svg files from temp to image folder.
-for file in os.listdir(svg_path):
-    shutil.move(os.path.join(svg_path, file), image_directory)
+# move from temp to destination directories.
+shutil.move(openmoji_svg_color_path, os.path.join(color_image_directory, "svg"))
+shutil.move(openmoji_svg_black_path, os.path.join(black_image_directory, "svg"))
+
+# update preview pages site.
+create_md_files(negarmoji_emoji_json, document_directory)
 
 # delete temp folder.
 process = subprocess.run(["rm", "-rf", f"{temp_directory}"], stdout=subprocess.PIPE)
